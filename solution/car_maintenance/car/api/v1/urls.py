de84052713f70ce5b4b7d91a408f5ca6f4ccd8fd
knowrafa <noprafa@gmail.com>
2021-10-11 03:29:43 -0300

    feature(API Routes): Creating Car API
    car/
    
    and draft for nested tyre route

--- solution/car_maintenance/car/api/v1/urls.py
+++ solution/car_maintenance/car/api/v1/urls.py
@@ -1,4 +1,15 @@
 from django.urls import include, path
+from rest_framework_nested import routers
+from . import views
+
+router = routers.DefaultRouter()
+
+router.register('cars', views.CarViewSet, basename="car")
+
+car_router = routers.NestedSimpleRouter(router, 'cars', lookup='car')
+# car_router.register('tyres', views.TyreViewSet, basename='tyre')
 
 urlpatterns = [
+    path("", include(router.urls)),
+    path("car_router", include(car_router.urls)),
 ]
