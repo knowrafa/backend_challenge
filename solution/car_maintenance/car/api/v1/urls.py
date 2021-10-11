9c10b9ea8143829e5ff3d21bc2fd9f7fa8664245
knowrafa <noprafa@gmail.com>
2021-10-11 17:24:58 -0300

    feature(Creating tyre URL):

--- solution/car_maintenance/car/api/v1/urls.py
+++ solution/car_maintenance/car/api/v1/urls.py
@@ -4,12 +4,11 @@ from . import views
 
 router = routers.DefaultRouter()
 
-router.register('cars', views.CarViewSet, basename="car")
-
-car_router = routers.NestedSimpleRouter(router, 'cars', lookup='car')
-# car_router.register('tyres', views.TyreViewSet, basename='tyre')
+router.register("cars", views.CarViewSet, basename="car")
+router.register("tyres", views.TyreViewSet, basename="tyres")
+car_router = routers.NestedSimpleRouter(router, "cars", lookup="car")
+car_router.register("tyres", views.TyreViewSet, basename="tyre")
 
 urlpatterns = [
     path("", include(router.urls)),
-    path("car_router", include(car_router.urls)),
 ]
