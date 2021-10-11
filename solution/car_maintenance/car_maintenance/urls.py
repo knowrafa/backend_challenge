cee3ba15897b608dce1a489a7a8473f35499cad2
knowrafa <noprafa@gmail.com>
2021-10-11 02:08:31 -0300

    feature(Creating Swagger URLS): Adding swagger and redoc urls

--- solution/car_maintenance/car_maintenance/urls.py
+++ solution/car_maintenance/car_maintenance/urls.py
@@ -15,8 +15,27 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Backend Challenge API",
+        default_version="v1",
+        description="Swagger documentation for API",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("car/", include("car.urls")),
+    path("", include("car.urls")),
+    path(
+        "docs/swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "docs/redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
