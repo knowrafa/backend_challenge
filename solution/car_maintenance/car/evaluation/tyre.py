e094a6499a5bfd47c090d61da3c6cc5044cf8b49
knowrafa <noprafa@gmail.com>
2021-10-11 02:56:56 -0300

    chore(Removing unused folder): Deleting car/evaluation/ module

--- solution/car_maintenance/car/evaluation/tyre.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-
-
-def verify_max_active_tyres(tyre):
-    if tyre.is_active and tyre.car.tyres.exclude(pk=tyre.pk).filter(active=True).count() == 4:
-        raise ValidationError({
-            "invalid": "This car already have 4 active tyres",
-        })
